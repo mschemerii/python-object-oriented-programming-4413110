74ae065dd1c9adda8e84d37c3cfd60838e0b1c0a
Michael Schemer <mschemerii@gmail.com>
2026-01-22 13:52:26 -0600

    22jan update

--- Start/Ch 1/definition_start.py
+++ Start/Ch 1/definition_start.py
@@ -2,10 +2,17 @@
 # Basic class definitions
 
 
-# TODO: create a basic class
+# create a basic class
+class Book:
+    """A class representing a book with a title and an author."""
+    def __init__(self, title, author):
+        self.title = title
+        self.author = author
 
+# create instances of the class
+book1 = Book("1984", "George Orwell")
+book2 = Book("To Kill a Mockingbird", "Harper Lee")
 
-# TODO: create instances of the class
-
-
-# TODO: print the class and property
+# print the class and property
+print(f"Book 1: {book1.title} by {book1.author}")
+print(f"Book 2: {book2.title} by {book2.author}")
